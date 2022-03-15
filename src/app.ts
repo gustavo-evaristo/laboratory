c2a8c20f750853a6b80b567063da19bb8a902164
Gustavo Evaristo <gug.henri1@gmail.com>
2022-03-14 21:28:46 -0300

    refactor: app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,32 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import '@database';
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
 import routes from './routes';
 
-const app = express();
+class App {
+  express: Application;
 
-app.use(routes);
+  constructor() {
+    this.express = express();
 
-export default app;
+    this.routes();
+  }
+
+  private routes(): void {
+    this.express.use(routes);
+
+    this.express.use((err: Error, request: Request, response: Response) => {
+      if (err instanceof Error) {
+        return response.status(406).json({
+          error: err.message,
+        });
+      }
+      return response.status(500).json({
+        error: `Internal server error - ${err}`,
+      });
+    });
+  }
+}
+
+export default new App().express;
